672d61dbc79a0ec2db0156ad18e1592c222486d2
payal <bharatrawal.br998877@gmail.com>
2024-08-01 17:01:43 +0530

    add

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://transaction-app-4.onrender.com/';
 
 const register = async (userData) => {
   const response = await axios.post(`${API_URL}/register`, userData);
